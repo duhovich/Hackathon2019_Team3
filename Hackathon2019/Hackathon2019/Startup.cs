a2fe8a4d177cd4055e7c61ce2b61eb7e6c112d1b
duhovich <olegduhovichrus@gmail.com>
2019-02-06 18:06:56 +0200

    Створив новий проект

---
+++ Hackathon2019/Hackathon2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackathon2019.Startup))]
+namespace Hackathon2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
