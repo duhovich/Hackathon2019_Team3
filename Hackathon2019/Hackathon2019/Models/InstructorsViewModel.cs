7d77af0f3e864fee792fe374bf0d61a9a9f71149
Filin69 <kolssnik.vovan@gmail.com>
2019-02-09 15:07:02 +0200

    Добавив контролер викладачів

---
+++ Hackathon2019/Hackathon2019/Models/InstructorsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon2019.Models
+{
+    public class InstructorsViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<string> FixedСourses { get; set; }
+    }
+}
